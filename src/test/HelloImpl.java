ad83ab92f8c17d0eb6991d4ad5545f244bae6c63
ma37060605 <ma37060605@126.com>
2015-04-20 08:32:20 +0800

    webServiceServer first import

---
+++ src/test/HelloImpl.java
@@ -0,0 +1,18 @@
+package test;
+/** 
+ * @author machao 
+ * @version 创建时间：2015-4-16 下午3:39:20 
+ * 程序的简单说明 
+ */
+public class HelloImpl implements IHello {
+
+	@Override
+	public String sayHello(String name) {
+		/**
+		 * @author  mc
+		 */
+		// TODO Auto-generated method stub
+		return "hello "+name;
+	}
+
+}
---
+++ src/test/IHello.java
@@ -0,0 +1,9 @@
+package test;
+/** 
+ * @author machao 
+ * @version 创建时间：2015-4-16 下午3:38:55 
+ * 程序的简单说明 
+ */
+public interface IHello {
+	public String sayHello(String name);
+}
